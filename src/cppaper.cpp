3d2babfa529eca3b2cda004adf3f2519ed41bc3a
Yuri dos Santos <yrds96@protonmail.com>
2022-11-07 16:36:28 -0300

    Added cppaper namespace

--- src/cppaper.cpp
+++ src/cppaper.cpp
@@ -9,216 +9,222 @@
 #include <map>
 
 
-using configMap = std::map<std::string, std::string>;
+namespace cppaper {
+
+  using configMap = std::map<std::string, std::string>;
 
 
-struct Page {
-  std::string title;
-  std::string html;
-  std::filesystem::path path;
-  configMap config;
-};
+  struct Page {
+    std::string title;
+    std::string html;
+    std::filesystem::path path;
+    configMap config;
+  };
 
-struct Directory {
-  std::filesystem::path path;
-  configMap config;
-  std::vector<Page> pages;
-};
+  struct Directory {
+    std::filesystem::path path;
+    configMap config;
+    std::vector<Page> pages;
+  };
 
-struct Site {
-  configMap config;
-  //std::vector<Directory> directories;
-  Directory directory;
-};
+  struct Site {
+    configMap config;
+    //std::vector<Directory> directories;
+    Directory directory;
+  };
 
-configMap getConfig(std::filesystem::path& directory) {
-  configMap directoryConfig;
+  configMap getConfig(std::filesystem::path& directory) {
+    configMap directoryConfig;
 
-  std::ifstream configFile;
+    std::ifstream configFile;
 
-  if(std::filesystem::is_directory(directory)) {
-    configFile.open(directory.string() + "/config");
-  } else {
-    configFile.open(directory.string() + ".config");
-  }
+    if(std::filesystem::is_directory(directory)) {
+      configFile.open(directory.string() + "/config");
+    } else {
+      configFile.open(directory.string() + ".config");
+    }
 
-  if(configFile.is_open()) {
-    std::string line;
-    while(std::getline(configFile, line)) {
-      auto delimiterPosition = line.find("=");
-      if(delimiterPosition != std::string::npos) {
-        directoryConfig[line.substr(0, delimiterPosition)] = line.substr(delimiterPosition + 1);
+    if(configFile.is_open()) {
+      std::string line;
+      while(std::getline(configFile, line)) {
+        auto delimiterPosition = line.find("=");
+        if(delimiterPosition != std::string::npos) {
+          directoryConfig[line.substr(0, delimiterPosition)] = line.substr(delimiterPosition + 1);
+        }
       }
     }
-  }
 
 
-  for(const auto& [config, value]: directoryConfig) {
-    std::cout << config << ":" << value << std::endl;
+    for(const auto& [config, value]: directoryConfig) {
+      std::cout << config << ":" << value << std::endl;
 
-  }
-  return directoryConfig;
-}
-
-Directory getPagesDirectory(std::string pagesDirectoryPath = "pages") {
-  
-  //TODO exception if pages is not a directory
-  std::filesystem::path directoryPath{pagesDirectoryPath};
-
-  return {
-    std::filesystem::path(pagesDirectoryPath),
-    getConfig(directoryPath)
-  };
-}
-
-std::vector<std::filesystem::path> getMdFiles(std::filesystem::path dirPath) {
-  //TODO dirPath must be a dir, otherwise exception
-  std::vector<std::filesystem::path> mdFiles;
-
-  for(auto const& dirEntry : std::filesystem::directory_iterator{dirPath}) {
-    if(dirEntry.path().extension().string() == ".md") {
-      mdFiles.push_back(dirEntry.path());
     }
+    return directoryConfig;
   }
 
-  return mdFiles;
-}
+  Directory getPagesDirectory(std::string pagesDirectoryPath = "pages") {
 
-std::vector<std::filesystem::path> getDirectories(std::filesystem::path dirPath) {
+    //TODO exception if pages is not a directory
+    std::filesystem::path directoryPath{pagesDirectoryPath};
 
-  std::vector<std::filesystem::path> directories;
+    return {
+      std::filesystem::path(pagesDirectoryPath),
+        getConfig(directoryPath)
+    };
+  }
 
-  directories.push_back(dirPath);
+  std::vector<std::filesystem::path> getMdFiles(std::filesystem::path dirPath) {
+    //TODO dirPath must be a dir, otherwise exception
+    std::vector<std::filesystem::path> mdFiles;
 
-  for(auto const& dirEntry : std::filesystem::directory_iterator{dirPath}) {
-    if(std::filesystem::is_directory(dirEntry)) {
-      directories.push_back(dirEntry.path());
+    for(auto const& dirEntry : std::filesystem::directory_iterator{dirPath}) {
+      if(dirEntry.path().extension().string() == ".md") {
+        mdFiles.push_back(dirEntry.path());
+      }
     }
+
+    return mdFiles;
   }
 
-  return directories;
-}
+  std::vector<std::filesystem::path> getDirectories(std::filesystem::path dirPath) {
 
-std::filesystem::path createPublicDirectory(bool overrideDirectory = true) {
-  const std::string publicDirectoryPath = "public";
+    std::vector<std::filesystem::path> directories;
 
-  std::filesystem::path publicDirectory(publicDirectoryPath);
+    directories.push_back(dirPath);
 
-  if(std::filesystem::exists(publicDirectory) && overrideDirectory) {
-    std::filesystem::remove_all(publicDirectory);
+    for(auto const& dirEntry : std::filesystem::directory_iterator{dirPath}) {
+      if(std::filesystem::is_directory(dirEntry)) {
+        directories.push_back(dirEntry.path());
+      }
+    }
+
+    return directories;
   }
 
-  //TODO exception if directory couldn't be created
-  std::filesystem::create_directory(publicDirectoryPath);
+  std::filesystem::path createPublicDirectory(bool overrideDirectory = true) {
+    const std::string publicDirectoryPath = "public";
 
-  return publicDirectory; 
-}
+    std::filesystem::path publicDirectory(publicDirectoryPath);
 
-std::string getHTMLFromMD(std::filesystem::path& mdFilePath) {
+    if(std::filesystem::exists(publicDirectory) && overrideDirectory) {
+      std::filesystem::remove_all(publicDirectory);
+    }
 
-  std::ifstream mdFile(mdFilePath, std::ios::binary);
+    //TODO exception if directory couldn't be created
+    std::filesystem::create_directory(publicDirectoryPath);
 
-  //TODO Find a better way to write this code(It's a mess!)
-  if(mdFile.is_open()) {
-    std::stringstream ss;
-
-    ss << mdFile.rdbuf();
-
-    auto mdContent = ss.str();
-
-    auto htmlContent = std::string{cmark_markdown_to_html(mdContent.c_str(), mdContent.size(), 0)};
-
-    return htmlContent;
-  } 
-
-  throw("Was not possible to open mdFile");
-}
-
-
-std::vector<Page> getPages(std::filesystem::path& directory) {
-  std::vector<Page> pages;
-
-  for(auto& mdFile: getMdFiles(directory)) {
-    pages.push_back({
-        mdFile.stem(),
-        getHTMLFromMD(mdFile),
-        mdFile,
-        getConfig(mdFile)
-        });
+    return publicDirectory; 
   }
 
-  return pages;
-}
+  std::string getHTMLFromMD(std::filesystem::path& mdFilePath) {
 
-std::string execTemplate(std::string templateFilePath, const Page& page) { //TODO add Site& reference
-  auto templateFile = std::ifstream(templateFilePath);
+    std::ifstream mdFile(mdFilePath, std::ios::binary);
 
-  std::stringstream templateStream;
-
-  templateStream << templateFile.rdbuf();
-
-  std::string templateString = templateStream.str();
-
-  templateString.replace(templateString.find("{{ $html }}"), 12, page.html);
-
-  return templateString;
-}
-
-void outputPages(Directory& pagesDirectory, std::filesystem::path& publicDirectory) {
-  for(const auto& page: pagesDirectory.pages) {
-    auto pagePath = page.path;
-
-    auto fileName = pagePath.replace_extension(".html").string().replace(page.path.string().find(pagesDirectory.path.string()), pagesDirectory.path.string().size(), publicDirectory.string());
-
-    std::cout << "Writing: " + fileName << std::endl;
-
-    std::ofstream outputPageFile(fileName);
-
-    if(outputPageFile.is_open()) {
+    //TODO Find a better way to write this code(It's a mess!)
+    if(mdFile.is_open()) {
       std::stringstream ss;
 
-      if(pagesDirectory.config.contains("template")) {
+      ss << mdFile.rdbuf();
 
-        ss << execTemplate("templates/" + pagesDirectory.config.at("template"), page);
+      auto mdContent = ss.str();
 
-      } else {
-        ss << page.html;
-      }
+      auto htmlContent = std::string{cmark_markdown_to_html(mdContent.c_str(), mdContent.size(), 0)};
 
-      outputPageFile << ss.rdbuf();
-    } else {
-      throw("Can't open file");
+      return htmlContent;
+    } 
+
+    throw("Was not possible to open mdFile");
+  }
+
+
+  std::vector<Page> getPages(std::filesystem::path& directory) {
+    std::vector<Page> pages;
+
+    for(auto& mdFile: getMdFiles(directory)) {
+      pages.push_back({
+          mdFile.stem(),
+          getHTMLFromMD(mdFile),
+          mdFile,
+          getConfig(mdFile)
+          });
     }
 
+    return pages;
   }
-}
+
+  std::string execTemplate(std::string templateFilePath, const Page& page) { //TODO add Site& reference
+    auto templateFile = std::ifstream(templateFilePath);
+
+    std::stringstream templateStream;
+
+    templateStream << templateFile.rdbuf();
+
+    std::string templateString = templateStream.str();
+
+    templateString.replace(templateString.find("{{ $html }}"), 12, page.html);
+
+    return templateString;
+  }
+
+  void outputPages(Directory& pagesDirectory, std::filesystem::path& publicDirectory) {
+    for(const auto& page: pagesDirectory.pages) {
+      auto pagePath = page.path;
+
+      auto fileName = pagePath.replace_extension(".html").string().replace(page.path.string().find(pagesDirectory.path.string()), pagesDirectory.path.string().size(), publicDirectory.string());
+
+      std::cout << "Writing: " + fileName << std::endl;
+
+      std::ofstream outputPageFile(fileName);
+
+      if(outputPageFile.is_open()) {
+        std::stringstream ss;
+
+        if(pagesDirectory.config.contains("template")) {
+
+          ss << execTemplate("templates/" + pagesDirectory.config.at("template"), page);
+
+        } else {
+          ss << page.html;
+        }
+
+        outputPageFile << ss.rdbuf();
+      } else {
+        throw("Can't open file");
+      }
+
+    }
+  }
 
 
-//Load configuration and output directory files to public directory
-void outputSite(Site& site, std::filesystem::path& publicDirectory) {
-  outputPages(site.directory, publicDirectory);
-}
+  //Load configuration and output directory files to public directory
+  void outputSite(Site& site, std::filesystem::path& publicDirectory) {
+    outputPages(site.directory, publicDirectory);
+  }
 
-Site getSite() {
-  std::filesystem::path sitePath("");
+  Site getSite() {
+    std::filesystem::path sitePath("");
 
-  auto pagesDirectory = getPagesDirectory(); 
+    auto pagesDirectory = getPagesDirectory(); 
 
-  return Site {
-    getConfig(sitePath),
-  };
-}
+    return Site {
+      getConfig(sitePath),
+    };
+  }
 
-void loadSiteDirectories(Site& site) {
+  void loadSiteDirectories(Site& site) {
     site.directory = getPagesDirectory();
-}
+  }
 
-void loadSitePages(Site& site) {
-  site.directory.pages = getPages(site.directory.path);
+  void loadSitePages(Site& site) {
+    site.directory.pages = getPages(site.directory.path);
+  }
+
 }
 
 int main(int argc, char *argv[])
 {
+
+  using namespace cppaper;
 
   auto site = getSite();
 
