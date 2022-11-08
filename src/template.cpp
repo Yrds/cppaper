15d24bc0416f4f60491ada70baaac83931d22648
Yuri dos Santos <yrds96@protonmail.com>
2022-11-08 13:36:35 -0300

    Code format

--- src/template.cpp
+++ src/template.cpp
@@ -11,15 +11,17 @@
 
 namespace cppaper {
 
-std::string execTemplate(std::string templateFilePath,
-                         const Page &page, const Directory& directory, const Site& site) { // TODO add Site& reference
+std::string execTemplate(std::string templateFilePath, const Page &page,
+                         const Directory &directory,
+                         const Site &site) { // TODO add Site& reference
 
-  inja::Environment env; //TODO maybe make env as a reference, so doesn't need to be included in every iteration with a page
+  inja::Environment env; // TODO maybe make env as a reference, so doesn't need
+                         // to be included in every iteration with a page
 
   inja::Template templ = env.parse_template(templateFilePath);
 
   inja::json data;
-  //TODO include partials layouts to be included
+  // TODO include partials layouts to be included
 
   data["page"]["html"] = page.html;
 
