56075ca8bf2b1522bb72d1fc7b07e56b6dd35264
Yuri dos Santos <yrds96@protonmail.com>
2022-12-13 19:53:49 -0300

    Added assets to public directory and changes to exmaple

--- src/cppaper.cpp
+++ src/cppaper.cpp
@@ -34,133 +34,6 @@
 
 namespace cppaper {
 
-Directory getPagesDirectory(std::string pagesDirectoryPath = "pages") {
-
-  std::filesystem::path directoryPath{pagesDirectoryPath};
-
-  if (std::filesystem::is_directory(directoryPath)) {
-    return {std::filesystem::path(pagesDirectoryPath),
-            getConfig(directoryPath)};
-  } else {
-    throw "Path is not a directory: " + pagesDirectoryPath;
-  }
-}
-
-std::vector<std::filesystem::path> getMdFiles(std::filesystem::path dirPath) {
-  if (std::filesystem::is_directory(dirPath)) {
-    std::vector<std::filesystem::path> mdFiles;
-
-    for (auto const &dirEntry : std::filesystem::directory_iterator{dirPath}) {
-      auto const pathExtension = dirEntry.path().extension().string();
-      if (pathExtension == ".md" || pathExtension == ".html") {
-        mdFiles.push_back(dirEntry.path());
-      }
-    }
-
-    return mdFiles;
-
-  } else {
-    throw "This directory it's not a path: " + dirPath.string();
-  }
-}
-
-std::vector<std::filesystem::path>
-getDirectories(std::filesystem::path dirPath) {
-  std::vector<std::filesystem::path> directories;
-
-  directories.push_back(dirPath);
-
-  for (auto const &dirEntry : std::filesystem::directory_iterator{dirPath}) {
-    if (std::filesystem::is_directory(dirEntry)) {
-      directories.push_back(dirEntry.path());
-    }
-  }
-
-  return directories;
-}
-
-std::filesystem::path createPublicDirectory(bool overrideDirectory = true) {
-  const std::string publicDirectoryPath = "public";
-
-  std::filesystem::path publicDirectory(publicDirectoryPath);
-
-  if (std::filesystem::exists(publicDirectory) && overrideDirectory) {
-    std::filesystem::remove_all(publicDirectory);
-  }
-
-  std::filesystem::create_directory(publicDirectoryPath);
-
-  return publicDirectory;
-}
-
-std::string getHTMLFromMD(std::filesystem::path &mdFilePath) {
-
-  std::ifstream mdFile(mdFilePath, std::ios::binary);
-
-  // TODO Find a better way to write this code(It's a mess!)
-  if (mdFile.is_open()) {
-    std::stringstream ss;
-
-    ss << mdFile.rdbuf();
-
-    auto mdContent = ss.str();
-
-    auto htmlContent = std::string{
-        cmark_markdown_to_html(mdContent.c_str(), mdContent.size(), 0)};
-
-    return htmlContent;
-  }
-
-  throw("Was not possible to open mdFile");
-}
-
-std::vector<Page> getPages(std::filesystem::path &directory) {
-  std::vector<Page> pages;
-
-  for (auto &mdFile : getMdFiles(directory)) {
-    pages.push_back({mdFile.stem(), mdFile, getConfig(mdFile)});
-    //{mdFile.stem(), getHTMLFromMD(mdFile), mdFile, getConfig(mdFile)});
-  }
-
-  return pages;
-}
-
-void outputPages(Directory &pagesDirectory,
-                 std::filesystem::path &publicDirectory, Site &site) {
-  for (const auto &page : pagesDirectory.pages) {
-    auto pagePath = page.path;
-
-    auto fileName = pagePath.replace_extension(".html").string().replace(
-        page.path.string().find(pagesDirectory.path.string()),
-        pagesDirectory.path.string().size(), publicDirectory.string());
-
-    std::cout << "Writing: " + fileName << std::endl;
-
-    std::ofstream outputPageFile(fileName);
-
-    if (outputPageFile.is_open()) {
-      std::stringstream ss;
-
-      if (pagesDirectory.config.contains("template")) {
-        ss << execTemplate(getTemplateDirectory().string() +
-                               pagesDirectory.config.at("template"),
-                           page, pagesDirectory, site);
-      } else {
-        // TODO ss << page.html;
-      }
-
-      outputPageFile << ss.rdbuf();
-    } else {
-      throw("Can't open file");
-    }
-  }
-}
-
-void outputSite(Site &site, std::filesystem::path &publicDirectory) {
-  // TODO Recursive paths to remove first parameter
-  outputPages(site.directory, publicDirectory, site);
-}
-
 Site getSite(entt::registry &registry) {
   const std::filesystem::path sitePath("");
 
@@ -256,10 +129,6 @@ void loadSiteFiles(entt::registry &registry) {
   });
 }
 
-void loadSitePages(Site &site) {
-  site.directory.pages = getPages(site.directory.path);
-}
-
 void generateContent(entt::registry &registry) {
   const auto markdownView =
       registry.view<const OriginPathComponent, PageContentComponent,
@@ -318,6 +187,16 @@ void loadConfig(entt::registry &registry) {
   });
 }
 
+
+void clearDirectory(std::filesystem::path directory) {
+  for (auto const &dirEntry : std::filesystem::directory_iterator{directory}) {
+    if (dirEntry.path().filename() != "assets") {
+      std::filesystem::remove_all(dirEntry.path());
+    }
+  }
+}
+
+
 void outputContent(entt::registry &registry) {
 
   const auto directoryView =
@@ -325,7 +204,9 @@ void outputContent(entt::registry &registry) {
 
   const std::filesystem::path pagesPath{"pages"};
 
-  std::filesystem::remove_all("public");
+  //TODO Remove all inside public but assets to preserve assets
+  clearDirectory(std::filesystem::path("public"));
+
   std::filesystem::create_directory("public");
 
   directoryView.each([&pagesPath](const auto &originPath) {
