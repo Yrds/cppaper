034622772e3dcea87d4a75bc4b4a5c28827394c0
Yuri dos Santos <yrds96@protonmail.com>
2022-12-17 22:19:50 -0300

    verify if generate_docs.sh are being called by meson

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 #
-"$MESON_BUILD_ROOT/cppaper" -C "$MESON_SOURCE_ROOT/docs_src" -O "$MESON_SOURCE_ROOT/docs"
+
+if [[ -z ${MESON_BUILD_ROOT} ]]; then
+  echo "This script must be runned by 'meson compile -C build docs' command"
+else
+  "$MESON_BUILD_ROOT/cppaper" -C "$MESON_SOURCE_ROOT/docs_src" -O "$MESON_SOURCE_ROOT/docs"
+fi
