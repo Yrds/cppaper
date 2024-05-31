9a60c0532be31069a94cc1a432570f10a40bf09e
Yuri dos Santos <yrds96@protonmail.com>
2024-05-31 03:02:03 -0300

    Fix generate docs not outputing to public

--- generate_docs.sh
+++ generate_docs.sh
@@ -5,5 +5,5 @@ if [[ -z ${MESON_BUILD_ROOT} ]]; then
   echo "This script must be runned by 'meson compile -C build docs' command"
 else
   cp $MESON_SOURCE_ROOT/README.md $MESON_SOURCE_ROOT/docs_src/pages/index.md
-  "$MESON_BUILD_ROOT/cppaper" -C "$MESON_SOURCE_ROOT/docs_src" -O "$MESON_SOURCE_ROOT/docs"
+  "$MESON_BUILD_ROOT/cppaper" -C "$MESON_SOURCE_ROOT/docs_src" -O "$MESON_SOURCE_ROOT/public"
 fi
